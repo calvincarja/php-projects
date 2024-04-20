91f5a2f073b0808b313521d6f9e3e085e7a57361
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-20 19:41:59 +0000

    learned about debugging and how it wont accept readline command

--- php-basic-exercises.php
+++ php-basic-exercises.php
@@ -1,4 +1,5 @@
 <?php
+    /*
     $num1 = 0;
     $num2 = 0;
     echo "enter your value for num1: ";
@@ -6,4 +7,14 @@
     echo "enter your value for num2: ";
     $num2 = (int) readline();
     echo "the sum of the two numers are: ". ($num1 + $num2);
+    */
+
+    $num1 = 0;
+    $num2 = 0;
+    echo "enter your value for num1: ";
+    $num1 = 10; // Temporarily replace readline() with a fixed value
+    echo "enter your value for num2: ";
+    $num2 = 7; // Temporarily replace readline() with a fixed value
+    echo "the sum of the two numers are: ". ($num1 + $num2);
+
 ?> 
