ce2d5edb0de2f0606c40eb5153e9cd941145dd9f
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-20 19:31:39 +0000

    peformed my first user input via the command line function

--- php-basic-exercises.php
+++ php-basic-exercises.php
@@ -1,3 +1,9 @@
 <?php
-    // my code will go here
+    $num1 = 0;
+    $num2 = 0;
+    echo "enter your value for num1: ";
+    $num1 = (int) readline();
+    echo "enter your value for num2: ";
+    $num2 = (int) readline();
+    echo "the sum of the two numers are: ". ($num1 + $num2);
 ?> 
